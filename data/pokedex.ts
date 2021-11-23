e00db8dc56c4634c9d2d87b4772d109acf77e3dc
legyllith <jeremy.n.49@live.fr>
2021-11-23 21:37:34 +0100

    test 2

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -17160,7 +17160,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 	},
 	flabebetradessar: {
 		num: 1819,
-		name: "test",
+		name: "test2",
 		types: ["Grass"],
 		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 50, atk: 70, def: 50, spa: 35, spd: 35, spe: 60},
