552a2dc03102ef17317315fb41fc2dba22cf6e0c
legyllith <jeremy.n.49@live.fr>
2021-12-23 10:31:58 +0100

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -1891,7 +1891,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 				this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[from] move: Burn Up');
 			},
 			onResidual(pokemon) {
-				if (!pokemon.hasType('Fire')) && this.field.isTerrain('burningterrain')) {
+				if (!pokemon.hasType('Fire') && this.field.isTerrain('burningterrain')) {
 					this.add('-start', target, 'typeadd', 'Fire', '[from] move: Burn Up');
 				} 
 			},
