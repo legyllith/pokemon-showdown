442a90418c4037a34c73aa82cc49dac867207dec
legyllith <jeremy.n.49@live.fr>
2022-04-01 14:34:34 +0200

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -2130,7 +2130,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {protect: 1, reflectable: 1, mirror: 1, allyanim: 1},
 		onModifyMove(move, pokemon) {
-			if (this.field.isTerrain('beachterrain')) move.boosts = {atk: -2, spa: -1};
+			if (this.field.isTerrain('beachterrain')) move.boosts = {atk: -2, spa: -2};
 		},
 		boosts: {
 			atk: -2,
