7bc46199910c4c9ffd30a21f4e18073525bdbf48
legyllith <jeremy.n.49@live.fr>
2022-04-18 19:47:13 +0200

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -4845,10 +4845,10 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 	walkman: {
 		// The Dive part of this mechanic is implemented in Dive's `onTryMove` in moves.ts
 		onSourceTryPrimaryHit(target, source, effect) {
-			if (effect.id === 'birdsong' && source.species.name === 'paritone') this.boost({spa: 1});
+			if (effect.id === 'birdsong' && source.species.name === 'Paritone') this.boost({spa: 1});
 			if (
 				effect && effect.id === 'birdsong' && source.hasAbility('walkman') &&
-				source.species.name === 'paritone'
+				source.species.name === 'Paritone'
 			) {
 				this.boost({spa: 1});
 				if (source.species.id !== 'paritone') source.formeChange('Paritone', effect);
