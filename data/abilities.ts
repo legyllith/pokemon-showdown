9d05c17aee243dc3e50462a7241c2a6e48bee69f
legyllith <jeremy.n.49@live.fr>
2022-04-27 18:27:49 +0200

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -4938,7 +4938,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			this.boost({atk: 1});
 			let horo = move.basePower;
 			if (move.category === 'Special') {:
-				let h = pokemon.volatiles['astrology']?.property
+				let h = pokemon.volatiles['astrology']?.property;
 				if (!(h >= 0 && h <= 12)) return;
                 		this.add('-activate', source, 'move: Astrology' + h);
 					if (h === 0) {
