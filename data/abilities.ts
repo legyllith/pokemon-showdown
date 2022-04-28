824f9799eb52e6a7e0f1f0afe9783c564e63bd17
legyllith <jeremy.n.49@live.fr>
2022-04-28 09:22:35 +0200

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -4933,12 +4933,12 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			this.add('-start', source, 'typeadd', 'Psychic', '[from] abilities: Horoscope');
 			source.addVolatile('astrology', this.effectState.target);
 		},
-		basePowerCallback: 23,
+		basePowerCallbackPriority: 23,
 		basePowerCallback(pokemon, target, move)  {
 			this.boost({atk: 1});
 			let horo = move.basePower;
 			if (move.category === 'Special') {
-				let h = pokemon.volatiles['astrology']?.property;
+				let h = pokemon.volatiles['astrology']?.hitCount;
 				if (!(h >= 0 && h <= 12)) return;
                 		this.add('-activate', source, 'move: Astrology' + h);
 					if (h === 0) {
