b34ff0c004d867de25c8c90f3a6145a6ed03eb8b
legyllith <jeremy.n.49@live.fr>
2022-05-25 13:57:11 +0200

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -21994,8 +21994,19 @@ export const Moves: {[moveid: string]: MoveData} = {
                 		this.add('-activate', source, 'move: Ceaseless Edge' + ' Your pokemon is endommaged' + this.effectState.type);
 				this.effectState.splinters = ((Math.floor(Math.floor(Math.floor(Math.floor(2 * source.level / 5 + 2) * 25 * source.getStat('atk', false, true)) / target.getStat('def', false, true)) / 50) +2)* Math.pow(2, typeMod));
 			},
-			onHit(pokemon, target, move) {
-				const damage = this.damage(this.effectState.splinters, pokemon, pokemon);
+			onModifyMove(pokemon, target, move) {
+				if(pokemon === target){
+					return;
+				}
+				if (!move.secondaries) {
+					move.secondaries = [];
+				}
+				move.secondaries.push({
+					chance: 100,
+					onHit(target, source, move) {
+						const damage = this.damage(this.effectState.splinters, pokemon, pokemon);
+					},
+				});
 			},
 		},
 		secondary: {
